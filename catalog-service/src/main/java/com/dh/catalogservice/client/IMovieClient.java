f9c959d67e3fb53dff993cc869d8b9a2c019f371
Your Name <laura.urrego.valois@gmail.com>
2023-08-17 21:28:45 -0500

    Se establece funcionalidad para crear películas.

--- catalog-service/src/main/java/com/dh/catalogservice/client/IMovieClient.java
+++ catalog-service/src/main/java/com/dh/catalogservice/client/IMovieClient.java
@@ -7,14 +7,19 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @FeignClient(name = "movie-service")
-@LoadBalancerClient(name= "movie-service", configuration = ConfiguracionBalanceador.class )
+@LoadBalancerClient(name = "movie-service", configuration = ConfiguracionBalanceador.class)
 public interface IMovieClient {
 
     @GetMapping("/api/v1/movies/{genre}")
     ResponseEntity<List<Movie>> getMoviesByGenre(@PathVariable String genre);
+
+    @PostMapping("/api/v1/movies/save")
+    ResponseEntity<Movie> saveMovie(@RequestBody Movie movie);
 
 }
--- catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
@@ -4,9 +4,7 @@ import com.dh.catalogservice.client.IMovieClient;
 import com.dh.catalogservice.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +18,11 @@ public class CatalogController {
     public ResponseEntity<List<Movie>> getCatalogByGenre(@PathVariable String genre) {
 
         return movieClient.getMoviesByGenre(genre);
+    }
+
+    @PostMapping("/api/v1/movies/save")
+    public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
+        return movieClient.saveMovie(movie);
     }
 
 }
