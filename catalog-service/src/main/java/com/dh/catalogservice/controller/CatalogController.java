89bbf53976c76e0660e92391ef0143c14c7a73e0
Your Name <laura.urrego.valois@gmail.com>
2023-09-13 16:51:58 -0500

    Se agregan archivos con screenshots, se agrega método en catalog-service que busca películas y series por género y se hacen modificaciones al circuit breaker.

--- catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
@@ -7,11 +7,15 @@ import com.dh.catalogservice.model.Serie;
 import com.dh.catalogservice.queue.Listener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @RestController
@@ -29,6 +33,7 @@ public class CatalogController {
     public CatalogController(Listener listener) {
         this.listener = listener;
     }
+
 
     @GetMapping("/catalog/movie/{genre}")
     public ResponseEntity<List<Movie>> getCatalogByGenre(@PathVariable String genre) {
@@ -57,5 +62,32 @@ public class CatalogController {
         listener.receiveSerie(serie);
         return serieClient.saveSerie(serie);
     }
+
+    /*---------------------------------------------------------------------------------*/
+
+    @GetMapping("/catalog/{genre}")
+    public ResponseEntity<Map<String, List<?>>> getMoviesAndSeriesByGenre(@PathVariable String genre) {
+        log.info("Solicitando películas y series por género: " + genre);
+
+        ResponseEntity<List<Movie>> moviesResponse = movieClient.getMoviesByGenre(genre);
+        ResponseEntity<List<Serie>> seriesResponse = serieClient.getSerieByGenre(genre);
+
+        Map<String, List<?>> catalog = new HashMap<>();
+
+        if (moviesResponse.getStatusCode().is2xxSuccessful()) {
+            catalog.put("movies", moviesResponse.getBody());
+        } else {
+            catalog.put("movies", Collections.emptyList());
+        }
+
+        if (seriesResponse.getStatusCode().is2xxSuccessful()) {
+            catalog.put("series", seriesResponse.getBody());
+        } else {
+            catalog.put("series", Collections.emptyList());
+        }
+
+        return ResponseEntity.ok(catalog);
+    }
+
 
 }
---
+++ catalog-service/src/main/java/com/dh/catalogservice/service/ISerieService.java
@@ -0,0 +1,10 @@
+package com.dh.catalogservice.service;
+
+import com.dh.catalogservice.model.Serie;
+
+import java.util.List;
+
+public interface ISerieService {
+
+    List<Serie> findAll();
+}
